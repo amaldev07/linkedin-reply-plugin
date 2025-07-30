80f90749c2c2d1333709678a901cd62afcf164fe
Amaldev <amaldev.psn@gmail.com>
2025-07-31 00:30:34 +0530

    update popup.js

--- popup.js
+++ popup.js
@@ -1,18 +1,14 @@
 document.getElementById('replyBtn').addEventListener('click', async () => {
-  // Run the content script's fillMessage function in the active tab
   chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       func: () => {
-        // This code will run in the context of the LinkedIn tab
-        // Find the user name (grab from DOM if possible)
-        // let userName = "there";
         let userName = document.querySelector('.msg-overlay-bubble-header__title a span')?.textContent.trim();
         let firstName = userName ? userName.split(' ')[0] : '';
 
         let input = document.querySelector('[contenteditable="true"][role="textbox"]');
         if (input) {
-          input.focus(); // Focus on the message box
+          input.focus();
             input.innerHTML = `<p>Hi ${firstName},</p><p><br><br></p><p>Regards,<br>Amaldev | amaldev.tech</p>`;
           input.dispatchEvent(new Event('input', { bubbles: true }));
         } else {
