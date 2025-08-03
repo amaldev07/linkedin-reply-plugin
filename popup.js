5d63b4a8efe1d6e8a2c6cd5bc5d009e613d7ab6c
Amaldev <amaldev.psn@gmail.com>
2025-08-03 12:25:54 +0530

    update popup.js

--- popup.js
+++ popup.js
@@ -1,8 +1,12 @@
 document.getElementById('replyBtn').addEventListener('click', async () => {
+  // Fetch the template first
+  const res = await fetch(chrome.runtime.getURL('message.txt'));
+  const template = await res.text();
+
   chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      func: () => {
+      func: (template) => {
         let userName = document.querySelector('.msg-overlay-bubble-header__title a span')?.textContent.trim();
         if (!userName) {
           userName = document.querySelector('.msg-entity-lockup__entity-title')?.textContent.trim();
@@ -12,12 +16,15 @@ document.getElementById('replyBtn').addEventListener('click', async () => {
         let input = document.querySelector('[contenteditable="true"][role="textbox"]');
         if (input) {
           input.focus();
-          input.innerHTML = `<p>Hi ${firstName},</p><p><br><br></p><p>Regards,<br>Amaldev | amaldev.tech</p>`;
+          message = decodeURIComponent(encodeURIComponent(template));
+          let msg = message.replace(/\$\{firstName\}/g, firstName).replace(/\n/g, '<br>');;
+          input.innerHTML = `<p>${msg}</p>`;
           input.dispatchEvent(new Event('input', { bubbles: true }));
         } else {
           alert('Message input not found!');
         }
-      }
+      },
+      args: [template]
     });
   });
 });
